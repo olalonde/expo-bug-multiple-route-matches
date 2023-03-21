f2dad20637274b0559a14106fc9612e33fd15cae
Oli Lalonde <olalonde@gmail.com>
2023-03-21 16:05:57 -0400

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { VStack, Text, ScrollView } from "native-base";
+
+
+export default function DashboardScreen() {
+  return (
+    <ScrollView>
+      <VStack space="md" alignItems="center" padding="4">
+        <Text>Hello world</Text>
+      </VStack>
+    </ScrollView>
+  );
+}
+
